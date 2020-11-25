07f53c17722cc5ff07f2f7bdfe77454f73f4afa2
Marmex Mander <vadim.m.lutsk@gmail.com>
2020-11-25 19:51:41 +0200

    Added class Position

---
+++ WorkDiary/WorkDiary/Models/Position.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WorkDiary.Models
+{
+    public class Position
+    {
+        int id;
+        int userId;
+        double wage;
+        string name;
+
+        public int Id { get => id; set => id = value; }
+        public int UserId { get => userId; set => userId = value; }
+        public double Wage { get => wage; set => wage = value; }
+        public string Name { get => name; set => name = value; }
+    }
+}
