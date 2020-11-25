4a766876ab3e6e4d99ed9b34cecec34c63e7568c
Viktor Davidenko <davidenko3015@gmail.com>
2020-11-25 19:41:28 +0200

    DiaryContext Add

---
+++ WorkDiary/WorkDiary/Models/DiaryContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WorkDiary.Models
+{
+    public class DiaryContext : DbContext
+    {
+        public DbSet<Object> Product { get; set; }
+
+        public DiaryContext(DbContextOptions<DiaryContext> options)
+            : base(options)
+        {
+
+            Database.EnsureCreated();
+        }
+    }
+}
