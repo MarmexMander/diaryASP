f950eba7b134f4c489b7ab08cd1792f702c9031d
Marmex Mander <vadim.m.lutsk@gmail.com>
2020-12-12 19:45:44 +0200

    Bug fixed: "Cookies don`t saving"

--- WorkDiary/WorkDiary/Models/SampleData.cs
+++ WorkDiary/WorkDiary/Models/SampleData.cs
@@ -15,7 +15,7 @@ namespace WorkDiary.Models
                     {
                         FullName = "Root Admin",
                         PassHash = "65E84BE33532FB784C48129675F9EFF3A682B27168C0EA744B2CF58EE02337C5",//qwerty
-                        AccessLevel = 3,
+                        AccessLevel = 2,
                         Email = "qwerty@qwe.asd"
                     }
                 );
