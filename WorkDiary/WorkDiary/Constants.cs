716dc8e4519c45821036e663ba6167d1a44c0d55
Marmex Mander <vadim.m.lutsk@gmail.com>
2020-12-25 21:45:15 +0200

    Added Constants

---
+++ WorkDiary/WorkDiary/Constants.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// ReSharper disable once CheckNamespace
+namespace WorkDiary.Constants
+{
+    public class LogMessages
+    {
+        public const string LOG_IN = "Logged In";
+        public const string LOG_OUT = "Logged Out";
+    }
+}
